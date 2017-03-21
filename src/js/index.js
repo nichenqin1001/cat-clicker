b708ca90b824ca526f90fd44ef5a80d88d0e0a18
nichenqin1001 <nichenqin@hotmail.com>
2017-03-21 16:25:32 +0800

    cat-clicker init

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var title = document.getElementById('title');
+    var title = document.getElementById('cat__title');
 
     title.innerText = 'Cat Clicker';
+
+    var cat = document.querySelector('.cat__img');
+    var counter = document.querySelector('.count__counter');
+    var num = counter.innerHTML;
+    cat.addEventListener('click', function () {
+        console.log(num);
+        num++;
+        counter.innerText = num;
+    });
 });
